87fd4d10c668b04f6734279a89679064f000b33b
LeiChen <261003307@qq.com>
2026-06-12 14:40:45 +0800

    ci: fix build scripts with --skip-install flag

--- packages/desktop/scripts/predev.ts
+++ packages/desktop/scripts/predev.ts
@@ -2,4 +2,4 @@ import { $ } from "bun"
 
 await $`bun ./scripts/copy-icons.ts ${process.env.OPENCODE_CHANNEL ?? "dev"}`
 
-await $`cd ../opencode && bun run script/build.ts --single`
+await $`cd ../opencode && bun run script/build.ts --single --skip-install`
